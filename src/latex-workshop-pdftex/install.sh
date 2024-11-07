6e5a03e023d402ffbb6f9b92986577039d4466b5
fdiesel <florian.diesel@posteo.de>
2024-11-07 16:22:10 +0000

    fix(features): option substitution

--- src/latex-workshop-pdftex/install.sh
+++ src/latex-workshop-pdftex/install.sh
@@ -6,7 +6,7 @@ mkdir -p $TMPDIR
 # Set the environment variables for the container
 echo SRC_DIR="$SRCDIR" >> $TMPDIR/env
 echo OUT_DIR="$OUTDIR" >> $TMPDIR/env
-echo TEXINPUTS_DIR="$TEXINPUTSDIR//:" >> $TMPDIR/env
+echo TEXINPUTS_DIR="$TEXINPUTS//:" >> $TMPDIR/env
 
 # Make the post script available
 cp post.sh $TMPDIR
