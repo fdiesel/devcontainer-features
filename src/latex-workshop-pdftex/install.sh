cbf27ee2c5382aac559e2d0bd2227e64fd400700
fdiesel <florian.diesel@posteo.de>
2024-11-07 15:02:55 +0000

    fix(features): option substitution

--- src/latex-workshop-pdftex/install.sh
+++ src/latex-workshop-pdftex/install.sh
@@ -1,1 +1,16 @@
-#!/bin/sh
+# configure-latex.sh
+#!/bin/bash
+
+# Variables from options
+SRC_DIR="${srcDir}"
+OUT_DIR="${outDir}"
+TEXINPUTS_DIR="${texInputs}"
+
+# Set the environment variables for the container
+export LATEX_SRC_DIR="/workspaces/$SRC_DIR"
+export LATEX_OUT_DIR="/workspaces/$OUT_DIR"
+export TEXINPUTS="/workspaces/$TEXINPUTS_DIR//:"
+
+# Create the necessary directories
+mkdir -p "$LATEX_SRC_DIR" "$LATEX_OUT_DIR"
+chmod -R 777 "$LATEX_SRC_DIR" "$LATEX_OUT_DIR"
