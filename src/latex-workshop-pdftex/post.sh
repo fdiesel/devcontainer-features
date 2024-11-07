8b5ca168baf1eab2f51679296fc96ca4b9a1ecde
fdiesel <florian.diesel@posteo.de>
2024-11-07 16:07:59 +0000

    fix(features): option substitution

--- src/latex-workshop-pdftex/post.sh
+++ src/latex-workshop-pdftex/post.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+
+WORKSPACE_DIR=$1
+SRC_DIR=$2
 TMPDIR=/tmp/latex-workshop-pdftex
 
 # Set the environment variables for the container
 source $TMPDIR/env
-echo "SRC_DIR=/workspaces/something/$SRC_DIR" >> ~/.bashrc
+echo "LATEX_SRC_DIR=$WORKSPACE_DIR/$SRC_DIR" >> ~/.bashrc
+echo "SRC_DIR=$SRC_DIR" >> ~/.bashrc
 
 # # Create the necessary directories
 # mkdir -p "/tmp/$LATEX_SRC_DIR" "/tmp/$LATEX_OUT_DIR"
